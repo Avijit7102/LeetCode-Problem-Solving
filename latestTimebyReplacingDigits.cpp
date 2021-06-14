2a14cb1c006477e76f0fcfc73e09c8ad643fe03a
Avijit Das <62779627+Avijit7102@users.noreply.github.com>
2021-06-14 18:46:17 -0400

    Easy-Problem-No-1736

---
+++ latestTimebyReplacingDigits.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string time = "1?:22";
+
+    if(time[0] == '?' && time[1] == '?')
+    {
+        time[0] = '2';
+        time[1] = '3';
+    }
+    if(time[0] == '?')
+    {
+        cout << "hit" << endl;
+        if(time[1] >= '4')
+        {
+            time[0] = '1';
+        }
+        else
+        {
+            time[0] = '2';
+        }
+    }
+    if(time[1] == '?')
+    {
+        if(time[0] == '1' || time[0] == '0')
+        {
+            time[1] = '9';
+        }
+        else
+        {
+            time[1] = '3';
+        }
+    }
+    if(time[3] == '?')
+    {
+        time[3] = '5';
+    }
+    if(time[4] == '?')
+    {
+        time[4] = '9';
+    }
+
+    cout << time << endl;
+}
